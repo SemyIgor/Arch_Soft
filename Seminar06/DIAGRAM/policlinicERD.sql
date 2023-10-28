c9467234659d87c93b6b5d77f49651727912b2e0
HenkSeed <adaner@inbox.ru>
2023-10-28 15:19:05 +0700

    HomeWork of the Seminar06 png

--- Seminar06/DIAGRAM/policlinicERD.sql
+++ Seminar06/DIAGRAM/policlinicERD.sql
@@ -1,28 +1,28 @@
 CREATE TABLE IF NOT EXISTS `person` (
-  `id` BIGINT NOT NULL PRIMARY KEY,
-  `FIO` VARCHAR(255) NOT NULL,
-  `adress` TEXT NOT NULL,
-  `birth_date` DATE NOT NULL,
-  `sex` CHAR(1) NOT NULL
+  `id` bigint NOT NULL PRIMARY KEY,
+  `FIO` varchar(255) NOT NULL,
+  `adress` text NOT NULL,
+  `birth_date` date NOT NULL,
+  `sex` char(1) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `patient` (
-  `id` BIGINT NOT NULL PRIMARY KEY,
-  `person_id` BIGINT NOT NULL,
-  `history_id` INT NOT NULL,
-  `OMS` BIGINT NOT NULL,
-  `DMS` BIGINT NOT NULL,
+  `id` bigint NOT NULL PRIMARY KEY,
+  `person_id` bigint NOT NULL,
+  `history_id` int NOT NULL,
+  `OMS` bigint NOT NULL,
+  `DMS` bigint NOT NULL,
   FOREIGN KEY (`person_id`) REFERENCES `person` (`id`),
   FOREIGN KEY (`history_id`) REFERENCES `history` (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `doctor` (
-  `id` BIGINT NOT NULL PRIMARY KEY,
-  `person_id` BIGINT NOT NULL,
-  `category_id` INT NOT NULL,
-  `specialization_id` BIGINT NOT NULL,
-  `room_id` INT NOT NULL,
-  `experience` DATETIME NOT NULL,
+  `id` bigint NOT NULL PRIMARY KEY,
+  `person_id` bigint NOT NULL,
+  `category_id` int NOT NULL,
+  `specialization_id` bigint NOT NULL,
+  `room_id` int NOT NULL,
+  `experience` datetime NOT NULL,
   FOREIGN KEY (`person_id`) REFERENCES `person` (`id`),
   FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
   FOREIGN KEY (`specialization_id`) REFERENCES `specialization` (`id`),
@@ -30,51 +30,67 @@ CREATE TABLE IF NOT EXISTS `doctor` (
 );
 
 CREATE TABLE IF NOT EXISTS `category` (
-  `id` INT NOT NULL PRIMARY KEY,
-  `category_name` VARCHAR(255) NOT NULL
+  `id` int NOT NULL PRIMARY KEY,
+  `category_name` varchar(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `specialization` (
-  `id` BIGINT NOT NULL PRIMARY KEY,
-  `specialization_name` VARCHAR(255) NOT NULL,
-  `specialization_code` BIGINT NOT NULL
+  `id` bigint NOT NULL PRIMARY KEY,
+  `specialization_name` varchar(255) NOT NULL,
+  `specialization_code` bigint NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `ICD_codes` (
-  `id` INT NOT NULL PRIMARY KEY,
-  `icd_code` VARCHAR(255) NOT NULL,
-  `disease_name` VARCHAR(255) NOT NULL,
-  `description` TEXT NOT NULL
+  `id` int NOT NULL PRIMARY KEY,
+  `icd_code` varchar(255) NOT NULL,
+  `disease_name` varchar(255) NOT NULL,
+  `description` text NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `history` (
-  `id` INT NOT NULL PRIMARY KEY,
-  `ICD_id` INT NOT NULL,
-  `disease_start` DATETIME NOT NULL,
-  `disease_stop` DATETIME NOT NULL,
-  `doctor_id` BIGINT NOT NULL,
-  `treatment` TEXT NOT NULL,
-  `recomendations` TEXT NOT NULL,
+  `id` int NOT NULL PRIMARY KEY,
+  `ICD_id` int NOT NULL,
+  `disease_start` datetime NOT NULL,
+  `disease_stop` datetime NOT NULL,
+  `doctor_id` bigint NOT NULL,
+  `tests_list_id` int NOT NULL,
+  `treatment` text NOT NULL,
+  `recomendations` text NOT NULL,
   FOREIGN KEY (`ICD_id`) REFERENCES `ICD_codes` (`id`),
-  FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`id`)
+  FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`id`),
+  FOREIGN KEY (`tests_list_id`) REFERENCES `tests_list` (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `rooms` (
-  `id` INT NOT NULL PRIMARY KEY,
-  `name` VARCHAR(255) NOT NULL
+  `id` int NOT NULL PRIMARY KEY,
+  `name` varchar(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `tests` (
-  `id` BIGINT NOT NULL PRIMARY KEY,
-  `test_name` VARCHAR(255) NOT NULL,
-  `laboratory_doctor_id` BIGINT NOT NULL,
-  `tests_list_id` INT NOT NULL,
+  `id` bigint NOT NULL PRIMARY KEY,
+  `test_name` varchar(255) NOT NULL,
+  `specialist_doctor_id` bigint NOT NULL,
+  `laboratory_doctor_id` bigint NOT NULL,
+  `tests_list_id` int NOT NULL,
+  FOREIGN KEY (`specialist_doctor_id`) REFERENCES `doctor` (`id`),
   FOREIGN KEY (`laboratory_doctor_id`) REFERENCES `doctor` (`id`),
   FOREIGN KEY (`tests_list_id`) REFERENCES `tests_list` (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `tests_list` (
-  `id` INT NOT NULL PRIMARY KEY,
-  `name` VARCHAR(255) NOT NULL,
-  `test_code` INT NOT NULL
+  `id` int NOT NULL PRIMARY KEY,
+  `name` varchar(255) NOT NULL,
+  `test_code` int NOT NULL,
+  `description` text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `schedule` (
+  `id` bigint NOT NULL PRIMARY KEY,
+  `patient_id` bigint NOT NULL,
+  `date` datetime NOT NULL,
+  `doctor_id` bigint NOT NULL,
+  `rooms_id` int NOT NULL,
+  FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`),
+  FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`id`),
+  FOREIGN KEY (`rooms_id`) REFERENCES `rooms` (`id`)
 );
